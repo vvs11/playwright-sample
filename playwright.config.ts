32dc7fc63bdce9907262dd243c9a97e0d35b8290
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2023-05-22 16:33:11 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,13 +22,13 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout: 5 * 60 * 1000,
+  timeout: 3 * 60 * 1000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
